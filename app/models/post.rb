db896f49156231a3764928481c27fd15bb24338f
Bobby Hinton <bob.g.hint@gmail.com>
2023-02-09 13:30:15 -0600

    commit tree 51055ab52ec0ea76071d4d4bcd8a979b366821a9

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,41 @@
 class Post < ApplicationRecord
+
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: {in:['Fiction', 'Non-Fiction']}
+
+  validate :click_bait_title
+
+  def click_bait_title
+    # takes the title attribute
+    # checks if the title includes 'Wont Believe', 'Secret', 'Top (some number)', 'Guess'
+    # if not, then we need to display an error message 
+
+    phrases = ["Won't Believe", "Secret","Top /\d/", "Guess"]
+
+    if title
+      is_valid = phrases.map do |phrase|
+                    title.include?(phrase)
+                  end.any?(true)
+    end
+
+    if !is_valid
+      errors.add(:title, "This title is not click-baity enough")
+    end
+
+
+
+    # phrases = [/Won't Believe/i,/Secret/i,/Top \d/i,/Guess/i]
+
+    # matches = phrases.none? { |phrase| phrase.match(title) }
+
+    # matches = phrases.any? { | phrase | self.title.include?(phrase) }
+
+    # matches = phrases.include? (title)
+
+    # if !matches
+    #   errors.add(:title, "This title is not click-baity enough")
+    # end
+  end
 end
